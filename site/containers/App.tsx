7f18df002748a599541d01349012255f3ff83b02
Robikz <zalewapl@gmail.com>
2018-11-11 13:41:20 +0100

    fix unnecessary compliculations

--- site/containers/App.tsx
+++ site/containers/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
 
-export class App extends Component {
+export default class App extends Component {
     render () {
         return <p>This is my new react app</p>
     }
--- site/index.tsx
+++ site/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 // main app
-import {App} from './containers/App';
+import App from './containers/App';
 require('./index.html');
 
 ReactDOM.render(<App />, document.getElementById('app'))
