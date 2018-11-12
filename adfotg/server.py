ca7dfe49342b39a782e4a5a2a4c8cac1fc9bd038
Robikz <zalewapl@gmail.com>
2018-11-12 11:34:30 +0100

    handle 404->500 on index.html in a generic way

--- adfotg/server.py
+++ adfotg/server.py
@@ -1,23 +1,29 @@
 from adfotg import app
+
 from flask import abort, request, send_from_directory
+from werkzeug.exceptions import NotFound
+
 import os.path
 
 
 @app.route('/', defaults={'path': 'index.html'})
 @app.route('/<path:path>')
 def serve_file(path):
-    if os.path.isdir(os.path.join(app.root_path, "site")):
-        # This is valid for deployment.
-        return send_from_directory("site", path)
-    elif os.path.isfile(os.path.join(app.root_path, "../setup.py")):
-        # Assume development mode and try to send from repository.
-        return send_from_directory("../site/dist", path)
-    else:
-        if path == "index.html":
-            return abort(500, "index.html should really have been found."
-                         " Your installation is broken")
+    try:
+        if os.path.isdir(os.path.join(app.root_path, "site")):
+            # This is valid for deployment.
+            return send_from_directory("site", path)
+        elif os.path.isfile(os.path.join(app.root_path, "../setup.py")):
+            # Assume development mode and try to send from repository.
+            return send_from_directory("../site/dist", path)
         else:
             return abort(404)
+    except NotFound as e:
+        if path == "index.html":
+            return abort(500, "index.html should have really been found."
+                         " Your installation is broken")
+        else:
+            raise
 
 
 @app.route("/upload", methods=['POST'])
