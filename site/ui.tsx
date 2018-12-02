5023c5c6940161d705010cbec09ca1730e01343c
Robikz <zalewapl@gmail.com>
2018-12-02 17:19:51 +0100

    Style listings

--- site/ui.tsx
+++ site/ui.tsx
@@ -43,12 +43,14 @@ interface ListingProps {
 export const Listing = (props: ListingProps) => {
 	let lines: JSX.Element[] = [];
 	if (props.listing) {
+		const customEntryClass = (props.className ? props.className + "__entry" : "");
 		props.listing.forEach((entry: string) => {
-			lines.push(<li key={entry}>{entry}</li>);
+			lines.push(<li className={"listing__entry " + customEntryClass}
+				key={entry}>{entry}</li>);
 		});
 	}
 	if (lines.length > 0) {
-		return (<ul className={"listing " + props.className}>
+		return (<ul className={"listing " + (props.className ? props.className : "")}>
 			{lines}
 		</ul>);
 	} else {
