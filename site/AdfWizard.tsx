0b0b3dd7b75a4c7221a269b6017e1c5bf7c4b4a1
Robikz <zalewapl@gmail.com>
2018-12-04 10:41:49 +0100

    Form text inputs fill the whole form item cell

--- site/AdfWizard.tsx
+++ site/AdfWizard.tsx
@@ -257,11 +257,11 @@ class DiskForm extends Component<DiskFormProps, DiskFormState> {
 					note={this.state.nameError && {
 						type: NoteType.Error,
 						message: this.state.nameError}}>
-					<input value={name} type="text" className="text-input"
+					<input value={name} type="text" className="text-input form__widget--full"
 						onChange={e => props.onNameEdited(e.target.value)} />
 				</FormItem>
 				<FormItem label="Label">
-					<input value={label} type="text" className="text-input"
+					<input value={label} type="text" className="text-input form__widget--full"
 						onChange={e => props.onLabelEdited(e.target.value)} />
 				</FormItem>
 				<FormItem label="Contents">
