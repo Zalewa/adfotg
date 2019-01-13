44f66d128fc265e01752c96dc708c1a0c5973633
Robikz <zalewapl@gmail.com>
2019-01-13 12:46:27 +0100

    Use a looking glass icon on the search button instead of text

--- site/res.tsx
+++ site/res.tsx
@@ -5,4 +5,5 @@ function r(path: string) {
 export const arrow_down = r('arrow_down.png');
 export const arrow_up = r('arrow_up.png');
 export const loader = r('ajax-loader.gif');
+export const looking_glass = r('looking_glass.png');
 export const usb_icon_horz = r('usb_icon_horz.svg');
