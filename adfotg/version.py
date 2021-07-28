129129c991a75fd8f3ba7ed68b800e4955f1472c
Robikz <zalewapl@gmail.com>
2021-07-28 19:29:48 +0200

    Include 2021 in the year span

--- adfotg/version.py
+++ adfotg/version.py
@@ -1,5 +1,5 @@
 FULLNAME = "ADF On-The-Go"
 SHORTNAME = "adfotg"
 VERSION = "0.3.0a0"
-YEARS = (2018, 2019)
-YEARSPAN = "{} - {}".format(YEARS[0], YEARS[1])
+YEARS = [2018, 2019, 2021]
+YEARSPAN = "{} - {}".format(YEARS[0], YEARS[-1])
