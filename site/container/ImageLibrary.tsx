b290347d7fdc05a59bd02c7e0984f77e839a25fd
Robikz <zalewapl@gmail.com>
2021-11-14 18:02:19 +0100

    Use the Button UI component in ImageLibrary

--- site/container/ImageLibrary.tsx
+++ site/container/ImageLibrary.tsx
@@ -14,8 +14,7 @@ import { dispatchApiErrors, dispatchRequestError } from '../component/Notifier';
 import Pager, { Page } from '../component/Pager';
 import * as res from '../res';
 import { Section } from '../component/Section';
-import style from '../style.less';
-import { DeleteButton, Icon } from '../component/ui';
+import { Button } from '../component/ui';
 
 interface ImageLibraryProps {
 	onCreatedImage: ()=>void
@@ -75,11 +74,12 @@ export default class ImageLibrary extends Component<ImageLibraryProps, ImageLibr
 	private renderActions(): JSX.Element {
 		return (<Actions>
 			<ActionSet>
-				<button onClick={this.showCreateImage} className={style.button}
-					disabled={this.state.selection.length == 0}>Create Mount Image</button>
+				<Button onClick={this.showCreateImage}
+					disabled={this.state.selection.length == 0}
+					title="Create Mount Image" />
 			</ActionSet>
 			<ActionSet right={true}>
-				<DeleteButton
+				<Button purpose="delete"
 					disabled={this.state.selection.length == 0}
 					onClick={() => this.setState({deleteSelected: true})} />
 			</ActionSet>
@@ -106,10 +106,8 @@ export default class ImageLibrary extends Component<ImageLibraryProps, ImageLibr
 
 	@boundMethod
 	private renderFileActions(file: FileTableEntry): JSX.Element {
-		return (<button className={`${style.button} ${style.buttonTable} ${style.buttonIconTable}`}
-					onClick={() => this.quickMount(file.name)}>
-			<Icon table button title="Quick Mount" src={res.usb_icon_horz} />
-		</button>);
+		return (<Button table title="Quick mount" icon={res.usb_icon_horz}
+			onClick={() => this.quickMount(file.name)} />)
 	}
 
 	@boundMethod
