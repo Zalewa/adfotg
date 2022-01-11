Zalewa/adfotg

commit 5e4fd0f742f5016f02a54b87e5c45aa05823c996

Fix: disabled Button would change its appearance on click as if it was enabled

site/ui/Button.tsx
@@ -19,7 +19,7 @@ export const ButtonMixin = css(
 		display: "inline-block",
 		fontFamily: skin.fontFamily,
 		position: "relative",
-		'&:active': [
+		'&:active &:not(:disabled)': [
 			{
 				backgroundColor: skin.workbench.titleColor,
 			},

commit 376e0513a41bea4897ed95a7746f0296aafb63fa

Fix: enabled Button wouldn't change its appearance on click
Broken in: 5e4fd0f742f5016f02a54b87e5c45aa05823c996

site/ui/Button.tsx
@@ -19,7 +19,7 @@ export const ButtonMixin = css(
 		display: "inline-block",
 		fontFamily: skin.fontFamily,
 		position: "relative",
-		'&:active &:not(:disabled)': [
+		'&:active:enabled': [
 			{
 				backgroundColor: skin.workbench.titleColor,
 			},