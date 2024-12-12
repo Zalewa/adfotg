fdeed4c7c70a50cf6109337d81ae47e41e5bbfe6
Robikz <zalewapl@gmail.com>
2024-12-12 19:56:55 +0100

    chore: include 2024 in the year span
    (hey, at least I did something)

--- src/adfotg/version.py
+++ src/adfotg/version.py
@@ -3,7 +3,7 @@ import sys
 FULLNAME = "ADF On-The-Go"
 SHORTNAME = "adfotg"
 VERSION = "0.0.0"
-YEARS = [2018, 2019, 2021, 2022, 2023]
+YEARS = [2018, 2019, 2021, 2022, 2023, 2024]
 YEARSPAN = "{} - {}".format(YEARS[0], YEARS[-1])
 
 _metadata_fallback = sys.version_info < (3, 8, 1)
