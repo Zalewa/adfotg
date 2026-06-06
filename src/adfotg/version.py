4fcc995e428ec52d6d19269a8127474bdd4455c7
Zalewa <zalewapl@gmail.com>
2026-06-06 13:31:13 +0200

    chore: include year 2026 in version.py

--- src/adfotg/version.py
+++ src/adfotg/version.py
@@ -3,7 +3,7 @@ import sys
 FULLNAME = "ADF On-The-Go"
 SHORTNAME = "adfotg"
 VERSION = "0.0.0"
-YEARS = [2018, 2019, 2021, 2022, 2023, 2024]
+YEARS = [2018, 2019, 2021, 2022, 2023, 2024, 2026]
 YEARSPAN = "{} - {}".format(YEARS[0], YEARS[-1])
 
 from importlib.metadata import version, PackageNotFoundError
