24329ad6e6b532a07a422c0e819c99bfe54776db
Zalewa <zalewapl@gmail.com>
2026-06-06 12:57:37 +0200

    build: proxy /api at vite to localhost:41364
    In development mode, the backend server runs on port 41364 (AMIGA) as
    before, but the site runs on port 5173 (vite) now.
    
    To keep both functioning in development mode, the procedure is now to:
    
    1. Run backend server in one terminal.
    2. Run site in vite in another terminal.
    3. Connect with browser to the URL exposed by vite in point 2.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root: "site",
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:43164'
+    },
+  },
 })
